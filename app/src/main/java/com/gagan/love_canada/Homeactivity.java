7e2e1f7c1d72f07351b9c1714f9cba2df9fbf64e
gagandeep kaur <sherry2312.gk@gmail.com>
2020-04-06 23:20:52 -0400

    recyclerviewadapter

--- app/src/main/java/com/gagan/love_canada/Homeactivity.java
+++ app/src/main/java/com/gagan/love_canada/Homeactivity.java
@@ -3,12 +3,32 @@ package com.gagan.love_canada;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
 
 public class Homeactivity extends AppCompatActivity {
+    private static final String TAG = "Homeactivity";
+
+    private ArrayList<String> mNames = new ArrayList<>();
+    private ArrayList<String> mImageUrls = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homeactivity);
+        Log.d(TAG, "onCreate :Started.");
+    }
+
+    private void initImageBitmaps() {
+        Log.d(TAG, "  initImageBitmaps:preparing bitmaps.");
+        mImageUrls.add("https://media1.picsearch.com/is?kFbHfprJq4unGL6SXd6v5bL7JK_HozKD9cIoYo14VF4&height=161");
+        mNames.add("Nigara Fall");
+
+        mImageUrls.add("https://media4.picsearch.com/is?E9YZaJ4lwxO1RTHLeHbs7tNzuPYjQyTW3EeEpk6Z9A4&height=255");
+        mNames.add("CN tower");
+
+        mImageUrls.add("https://i.redd.it/tpsnoz5bzo501.jpg");
+        mNames.add("Trondheim");
     }
 }
--- app/src/main/java/com/gagan/love_canada/recyclerviewadapter.java
+++ app/src/main/java/com/gagan/love_canada/recyclerviewadapter.java
@@ -1,4 +1,81 @@
 package com.gagan.love_canada;
 
-public class recyclerviewadapter {
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.RecyclerView.ViewHolder;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class recyclerviewadapter extends RecyclerView.Adapter<recyclerviewadapter.viewholder> {
+    private static final String TAG = "recyclerviewadapter";
+    private ArrayList<String> mImagesNames = new ArrayList<>();
+    private ArrayList<String> mImages = new ArrayList<>();
+    private Context mContext;
+
+    public recyclerviewadapter(ArrayList<String> mImagesNames, ArrayList<String> mImages, Context mContext) {
+        this.mImagesNames = mImagesNames;
+        this.mImages = mImages;
+        this.mContext = mContext;
+    }
+
+
+    @Override
+    public recyclerviewadapter.viewholder onCreateViewHolder( ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from( parent.getContext()).inflate(R.layout.listattractions,parent,false);
+        viewholder holder = new viewholder(view) ;
+        return holder;
+
+    }
+
+    @Override
+    public void onBindViewHolder( viewholder holder, final int position) {
+        Log.d(TAG,"onBindViewHolder:called.");
+        Glide.with(mContext)
+                .asBitmap()
+                .load(mImages.get(position))
+                .into(holder.image);
+        holder.imagename.setText(mImagesNames.get(position));
+        holder.parentLayout.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG,"onClick:clicked on:"+ mImagesNames.get(position));
+                Toast.makeText(mContext,mImagesNames.get(position),Toast.LENGTH_SHORT).show();
+
+            }
+
+
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mImagesNames.size();
+    }
+
+    public class viewholder extends RecyclerView.ViewHolder {
+        CircleImageView image;
+        TextView imagename;
+        ConstraintLayout parentLayout;
+
+        public viewholder(View itemView) {
+            super(itemView);
+            image = itemView.findViewById(R.id.image);
+            imagename = itemView.findViewById(R.id.image_name);
+            parentLayout = itemView.findViewById(R.id.parent);
+        }
+    }
 }
