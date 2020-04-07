cd827e0e6d635b5fdd790547fbcd05f4e02d2179
gagandeep kaur <sherry2312.gk@gmail.com>
2020-04-06 21:10:45 -0400

    Initial commit splash screen

---
+++ app/src/main/java/com/gagan/love_canada/Homeactivity.java
@@ -0,0 +1,14 @@
+package com.gagan.love_canada;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Homeactivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homeactivity);
+    }
+}
