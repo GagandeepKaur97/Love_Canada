d8a512cebf0a17739fa68dbe2d1d1b73823609a1
gagandeep kaur <sherry2312.gk@gmail.com>
2020-04-07 00:03:42 -0400

    added images.

--- app/src/main/java/com/gagan/love_canada/Homeactivity.java
+++ app/src/main/java/com/gagan/love_canada/Homeactivity.java
@@ -33,6 +33,44 @@ public class Homeactivity extends AppCompatActivity {
 
         mImageUrls.add("https://i.redd.it/tpsnoz5bzo501.jpg");
         mNames.add("Trondheim");
+
+        mImageUrls.add("https://img.traveltriangle.com/blog/wp-content/tr:w-700,h-400/uploads/2017/08/Lake-Louise.jpg");
+        mNames.add("lake louise");
+
+        mImageUrls.add("https://img.traveltriangle.com/blog/wp-content/tr:w-700,h-400/uploads/2017/08/Whistler.jpg");
+        mNames.add("whistler ski resort");
+
+        mImageUrls.add("https://img.traveltriangle.com/blog/wp-content/tr:w-700,h-400/uploads/2017/08/Banff-National-Park2.jpg");
+        mNames.add("banff national park");
+
+        mImageUrls.add("https://img.traveltriangle.com/blog/wp-content/tr:w-700,h-400/uploads/2017/08/Stanley-Park-e1567324833962.jpg");
+        mNames.add("stanley park");
+
+        mImageUrls.add("https://img.traveltriangle.com/blog/wp-content/tr:w-700,h-400/uploads/2017/08/Butchart-Gardens.jpg");
+        mNames.add("butchart gardens") ;
+        mImageUrls.add("https://img.traveltriangle.com/blog/wp-content/tr:w-700,h-400/uploads/2019/11/Cape-Bretron.jpg");
+        mNames.add("cape briton");
+        //mImageUrls.add("https://img.traveltriangle.com/blog/wp-content/tr:w-700,h-400/uploads/2020/01/Forillon-National-Park.jpg");
+       // mNames.add(" forllion national park");
+        mImageUrls.add("https://img.traveltriangle.com/blog/wp-content/tr:w-700,h-400/uploads/2020/01/Peggy%E2%80%99s-Cove.jpg");
+        mNames.add("peggy cove");
+        mImageUrls.add("https://img.traveltriangle.com/blog/wp-content/tr:w-700,h-400/uploads/2020/01/Waterton-Lakes-National-Park.jpg");
+        mNames.add("waterton lakes");
+        mImageUrls.add("https://img.traveltriangle.com/blog/wp-content/tr:w-700,h-400/uploads/2020/01/Capilano-Suspension-Bridge.jpg");
+        mNames.add("capilano bridge");
+        //mImageUrls.add("https://img.traveltriangle.com/blog/wp-content/tr:w-700,h-400/uploads/2020/01/Jasper-National-Park.jpg");
+        //mNames.add("jasper national park");
+        //mImageUrls.add("https://img.traveltriangle.com/blog/wp-content/tr:w-700,h-400/uploads/2019/07/Prince_Edward_Island_3.jpg");
+       // mNames.add("prince edward island");
+        mImageUrls.add("https://img.traveltriangle.com/blog/wp-content/tr:w-700,h-400/uploads/2019/08/Parc-National-Du-Mont-Tremblant1.jpg");
+        mNames.add(" mont tremblant");
+        //mImageUrls.add("https://img.traveltriangle.com/blog/wp-content/tr:w-700,h-400/uploads/2020/01/Montmorency-Falls.jpg");
+       // mNames.add("Montmorency-Falls");
+        mImageUrls.add("https://img.traveltriangle.com/blog/wp-content/tr:w-700,h-400/uploads/2020/01/Columbia-Icefield-Skywalk.jpg");
+        mNames.add("icefield sktfield");
+        mImageUrls.add("https://img.traveltriangle.com/blog/wp-content/tr:w-700,h-400/uploads/2020/01/Mount-Logan.jpeg");
+        mNames.add("mount logan");
+
         initRecyclerview();
     }
     private void initRecyclerview(){
